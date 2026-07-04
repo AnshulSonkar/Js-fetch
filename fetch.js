611d89567f58806fed0157c906661cecde5cda22
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-07-04 23:10:54 +0530

    commit

--- fetch.js
+++ fetch.js
@@ -1,9 +1,9 @@
-let title = document.getElementById("title");
-let body = document.getElementById("body");
+// let title = document.getElementById("title");
+// let body = document.getElementById("body");
 
-fetch("https://jsonplaceholder.typicode.com/posts/1")
-  .then(res => res.json())
-  .then(data => {
-    title.textContent = data.title;
-    body.textContent = data.body;
-  });
+// fetch("https://jsonplaceholder.typicode.com/posts/1")
+//   .then(res => res.json())
+//   .then(data => {
+//     title.textContent = data.title;
+//     body.textContent = data.body;
+//   });
